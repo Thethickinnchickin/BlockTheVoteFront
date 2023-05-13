b9bfe76b8878c750e9ae5038d18117e37c62325a
Matt <matt.reiley@sjsu.edu>
2023-05-13 15:58:24 -0700

    NFT token claim contract added

--- app/pages/index.js
+++ app/pages/index.js
@@ -198,7 +198,7 @@ export default function Home() {
       </Head>
       <div className={styles.main}>
         <div>
-          <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
+          <h1 className={styles.title}>Welcome to Block The Vote!</h1>
           <div className={styles.description}>
             {/* Using HTML Entities for the apostrophe */}
             It&#39;s an NFT for group voting
